a342d2d077f938760b5a1ae9acbd7c710eb3a982
namratapatel21 <namratapatel2192@gmail.com>
2020-07-19 19:40:22 +0100

    new model and controller file

---
+++ todo/resources/views/navs/navbar.blade.php
@@ -0,0 +1,11 @@
+<!-- Image and text -->
+<nav class="navbar navbar-light" style="background-color: #b6dcfd;">
+    <a class="navbar-brand" href="#">
+      <img src="https://image.flaticon.com/icons/svg/2948/2948249.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+      TODO APPLICATION
+    </a>
+  </nav>
+
+
+
+  
